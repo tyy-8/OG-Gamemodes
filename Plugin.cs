6414924858b1c6a7d0decca18d2fc034bb99c3dd
Ty <122072344+tyy-8@users.noreply.github.com>
2025-02-21 10:31:21 +1100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla.Attributes;
+using Utilla.Models;
+
+namespace OGGamemodes
+{
+    [ModdedGamemode("battle_Paintbrawl", "BATTLE",BaseGamemode.Paintbrawl)]
+    [ModdedGamemode("hunt_Hunt", "HUNT",BaseGamemode.Hunt)]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        // witewwy nothing
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace OGGamemodes
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.ty.gorillatag.oggamemodes";
+        public const string Name = "OGGamemodes";
+        public const string Version = "1.0.0";
+    }
+}
